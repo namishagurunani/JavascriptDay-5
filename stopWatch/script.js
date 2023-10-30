3f5f2f123a85f5038ff6dc61e8a594c269f090a8
shivam gurunani <namisha.gurunani173261@gmail.com>
2023-10-30 17:56:46 +0530

    calculator added

--- stopWatch/script.js
+++ stopWatch/script.js
@@ -3,47 +3,49 @@ let stopBtn = document.getElementById('stop');
 let startBtn = document.getElementById('start');
 let resetBtn = document.getElementById('reset');
 
-let msec = 00;
-let secs = 00;
-let mins = 00;
+let msec = 0; // Change '00' to 0
+let secs = 0; // Change '00' to 0
+let mins = 0; // Change '00' to 0
 
 let timerId = null;
 
-startBtn.addEventListener('click', function(){
-    if(timerId !== null){
+startBtn.addEventListener('click', function() {
+    if (timerId !== null) {
         clearInterval(timerId);
     }
     timerId = setInterval(startTimer, 10);
-    startBtn.style.opacity ="0.4";
 });
 
-stopBtn.addEventListener('click', function(){
+stopBtn.addEventListener('click', function() {
     clearInterval(timerId);
-    stopBtn.style.opacity ="0.4";
 });
 
-resetBtn.addEventListener('click', function(){
+resetBtn.addEventListener('click', function() {
     clearInterval(timerId);
-    timerDisplay.innerHTML = `00 : 00 : 00`;
-    msec = secs = mins = 00;
+    msec = 0; // Reset msec to 0
+    secs = 0; // Reset secs to 0
+    mins = 0; // Reset mins to 0
+    updateTimerDisplay(); // Call the function to update the timer display
 });
 
-function startTimer(){
+function startTimer() {
     msec++;
-    if(msec == 100){
+    if (msec == 100) {
         msec = 0;
         secs++;
-        if(secs == 60){
+        if (secs == 60) {
             secs = 0;
-mins++;
+            mins++;
         }
     }
 
+    updateTimerDisplay(); // Call the function to update the timer display
+}
+
+function updateTimerDisplay() {
     let msecString = msec < 10 ? `0${msec}` : msec;
     let secsString = secs < 10 ? `0${secs}` : secs;
     let minsString = mins < 10 ? `0${mins}` : mins;
-    
 
     timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
-
 }
